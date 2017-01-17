b88b238225315fba47d9d546fa85b16112b5b7ab
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2017-01-16 19:05:51 -0500

    Update 'instagram/__init__.py' for Instagram

--- instagram/__init__.py
+++ instagram/__init__.py
@@ -1,14 +1,11 @@
-# -*- coding: utf-8 -*-
-
 """
-Discord API Wrapper
-~~~~~~~~~~~~~~~~~~~
+Instagram API Wrapper.
+~~~~~~~~~~~~~~~~~~~~~~
 
-A basic wrapper for the Discord API.
+A basic wrapper for the Instagram API.
 
 :copyright: (c) 2015-2016 Rapptz
 :license: MIT, see LICENSE for more details.
-
 """
 
 __title__ = 'discord'
